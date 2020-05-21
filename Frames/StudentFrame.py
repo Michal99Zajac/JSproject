9f295b74f4b84fc4327d8618105f51048d4f3bc2
Michal Zajac <michal.99.zajac@gmail.com>
2020-05-21 11:38:50 +0200

    added extend for tkinter and completed functions in studentframe

--- Frames/StudentFrame.py
+++ Frames/StudentFrame.py
@@ -1,23 +1,26 @@
 import tkinter as tk
-from tkinter import font as tkfont
-import tkinter.ttk as ttk
 
 from Tables.Student import Student
+
+from Frames.extendTk import MultiListBox
 
 class StudentPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(
-            self,
-            text="Student Page",
-            font=controller.title_font
-        )
-        label.pack(side=tk.TOP, fill=tk.X, pady=10)
+        self.main_label()
 
         self.student_listbox()
         self.refresh_button()
         self.buttons()
+
+    def main_label(self):
+        label = tk.Label(
+            self,
+            text="Student Page",
+            font=self.controller.title_font
+        )
+        label.pack(side=tk.TOP, fill=tk.X, pady=10)
 
     def buttons(self):
         #Return Home Button
@@ -26,6 +29,7 @@ class StudentPage(tk.Frame):
             text="Return to Home Page",
             command=lambda: self.controller.show_frame("StartPage")
         )
+        btn_return.pack()
         #Create Student Button
         btn_create = tk.Button(
             self,
@@ -44,21 +48,35 @@ class StudentPage(tk.Frame):
             text="Change Student",
             command=lambda: self.update_student()
         )
-        btn_return.pack()
+        
         btn_create.pack()
         btn_delete.pack()
         btn_change.pack()
+
 
     def student_listbox(self):
         self.f_student = tk.Frame(master=self)
         self.l_student = tk.Label(master=self.f_student, text="select student")
         self.l_student.pack()
-        self.list_students = MultiListBox(master=self.f_student)
+
+        data = [
+            ('id', 10),
+            ('name', 20),
+            ('second name', 20),
+            ('lastname', 20),
+            ('ssn', 20),
+            ('email', 20),
+            ('field of study', 30),
+            ('place of residence', 40)
+        ]
+
+        self.list_students = MultiListBox(master=self.f_student, data=data)
 
         self.refresh()
 
         self.list_students.pack()
         self.f_student.pack()
+
 
     def delete_student(self):
         idx = self.list_students.index(tk.ACTIVE)
@@ -70,22 +88,27 @@ class StudentPage(tk.Frame):
         del del_student
         self.restart()
 
+
     def update_student(self):
         idx = self.list_students.index(tk.ACTIVE)
         student = self.controller.students[idx]
-        
+       
         self.controller.frames["ChangeStudentPage"].set_student(student)
+        self.controller.frames["ChangeStudentPage"].fill_entry()
         self.controller.show_frame("ChangeStudentPage")
+
 
     def restart(self):
         self.refresh()
         self.controller.show_frame("StudentPage")
 
+
     def refresh_button(self):
-        self.f_refresh = tk.Frame(master=self)
-        self.b_refresh = tk.Button(master=self.f_refresh, text="refresh", command=lambda : self.restart())
-        self.b_refresh.pack()
-        self.f_refresh.pack()
+        f_refresh = tk.Frame(master=self)
+        f_refresh.pack()
+        btn_refresh = tk.Button(master=f_refresh, text="refresh", command=lambda : self.restart())
+        btn_refresh.pack()
+
 
     def refresh(self):
         self.list_students.delete(0, tk.END)
@@ -102,19 +125,16 @@ class StudentPage(tk.Frame):
                 )
             self.list_students.insert(i,output)
         
+
+
 class CreateStudentPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        label = tk.Label(
-            self,
-            text="Create Student",
-            font=self.controller.title_font
-        )
-        label.pack(side=tk.TOP, fill=tk.X, pady=10)
-
-        self.return_buttons()
+        self.main_label()
+        self.return_button()
+        self.home_button()
         self.name_entry()
         self.sec_name_entry()
         self.lastname_entry()
@@ -124,89 +144,123 @@ class CreateStudentPage(tk.Frame):
         self.field_listbox()
         self.submit()
 
-    def return_buttons(self):
+    def main_label(self):
+        label = tk.Label(
+            self,
+            text="Create Student",
+            font=self.controller.title_font
+        )
+        label.pack(side=tk.TOP, fill=tk.X, pady=10)
+
+
+    def home_button(self):
         btn_home = tk.Button(
             self,
             text="Return to Home Page",
             command=lambda:self.controller.show_frame("StartPage")
         )
+        btn_home.pack()
+
+    def return_button(self):
         btn_return = tk.Button(
             self,
             text="return",
             command=lambda:self.controller.show_frame("StudentPage")
         )
-        btn_home.pack()
         btn_return.pack()
 
+
     def name_entry(self):
-        self.f_name = tk.Frame(master=self)
-        self.l_name = tk.Label(master=self.f_name, text="name")
-        self.l_name.pack()
-        self.e_name = tk.Entry(master=self.f_name)
+        f_name = tk.Frame(master=self)
+        f_name.pack()
+
+        l_name = tk.Label(master=f_name, text="name")
+        l_name.pack()
+
+        self.e_name = tk.Entry(master=f_name)
         self.e_name.pack()
-        self.f_name.pack()
+        
+
 
     def sec_name_entry(self):
-        self.f_sec_name = tk.Frame(master=self)
-        self.l_sec_name = tk.Label(master=self.f_sec_name, text="second name")
-        self.l_sec_name.pack()
-        self.e_sec_name = tk.Entry(master=self.f_sec_name)
+        f_sec_name = tk.Frame(master=self)
+        f_sec_name.pack()
+
+        l_sec_name = tk.Label(master=f_sec_name, text="second name")
+        l_sec_name.pack()
+
+        self.e_sec_name = tk.Entry(master=f_sec_name)
         self.e_sec_name.pack()
-        self.f_sec_name.pack()
+
 
     def lastname_entry(self):
-        self.f_lastname = tk.Frame(master=self)
-        self.l_lastname = tk.Label(master=self.f_lastname, text="lastname")
-        self.l_lastname.pack()
-        self.e_lastname = tk.Entry(master=self.f_lastname)
+        f_lastname = tk.Frame(master=self)
+        f_lastname.pack()
+
+        l_lastname = tk.Label(master=f_lastname, text="lastname")
+        l_lastname.pack()
+
+        self.e_lastname = tk.Entry(master=f_lastname)
         self.e_lastname.pack()
-        self.f_lastname.pack()
+
 
     def ssn_entry(self):
-        self.f_ssn = tk.Frame(master=self)
-        self.l_ssn = tk.Label(master=self.f_ssn, text="ssn")
-        self.l_ssn.pack()
-        self.e_ssn = tk.Entry(master=self.f_ssn)
+        f_ssn = tk.Frame(master=self)
+        f_ssn.pack()
+
+        l_ssn = tk.Label(master=f_ssn, text="ssn")
+        l_ssn.pack()
+
+        self.e_ssn = tk.Entry(master=f_ssn)
         self.e_ssn.pack()
-        self.f_ssn.pack()
+
 
     def email_entry(self):
-        self.f_email = tk.Frame(master=self)
-        self.l_email = tk.Label(master=self.f_email, text="email")
-        self.l_email.pack()
-        self.e_email = tk.Entry(master=self.f_email)
+        f_email = tk.Frame(master=self)
+        f_email.pack()
+
+        l_email = tk.Label(master=f_email, text="email")
+        l_email.pack()
+
+        self.e_email = tk.Entry(master=f_email)
         self.e_email.pack()
-        self.f_email.pack()
+
 
     def place_entry(self):
-        self.f_place = tk.Frame(master=self)
-        self.l_place = tk.Label(master=self.f_place, text="place of residence")
-        self.l_place.pack()
-        self.e_place = tk.Entry(master=self.f_place)
+        f_place = tk.Frame(master=self)
+        f_place.pack()
+
+        l_place = tk.Label(master=f_place, text="place of residence")
+        l_place.pack()
+
+        self.e_place = tk.Entry(master=f_place)
         self.e_place.pack()
-        self.f_place.pack()
+
 
     def field_listbox(self):
-        self.f_field = tk.Frame(master=self)
-        self.l_field = tk.Label(master=self.f_field, text="field of study")
-        self.l_field.pack()
-        self.list_field = tk.Listbox(master=self.f_field)
-        
+        f_field = tk.Frame(master=self)
+        f_field.pack()
+
+        l_field = tk.Label(master=f_field, text="field of study")
+        l_field.pack()
+
+        self.list_field = tk.Listbox(master=f_field)  
         for i, field in enumerate(self.controller.fields):
             self.list_field.insert(i,field.get_name())
-        
         self.list_field.pack()
-        self.f_field.pack()
+
 
     def submit(self):
-        self.f_submit = tk.Frame(master=self)
-        self.sub_btn = tk.Button(
-            master=self.f_submit,
+        f_submit = tk.Frame(master=self)
+        f_submit.pack()
+
+        sub_btn = tk.Button(
+            master=f_submit,
             text="submit",
-            command=lambda:self.create_student()
+            command=lambda : self.create_student()
             )
-        self.sub_btn.pack()
-        self.f_submit.pack()
+        sub_btn.pack()
+        
 
     def create_student(self):
         temp_field = None
@@ -230,12 +284,14 @@ class CreateStudentPage(tk.Frame):
         self.controller.show_frame("StudentPage")
         self.controller.frames["StudentPage"].restart()
 
-class ChangeStudentPage(tk.Frame):
+
+class ChangeStudentPage(CreateStudentPage):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
+        CreateStudentPage.__init__(self, parent, controller)
         self.student = controller.students[0]
 
+
+    def main_label(self):
         label = tk.Label(
             self,
             text="Change Student",
@@ -243,104 +299,79 @@ class ChangeStudentPage(tk.Frame):
         )
         label.pack(side=tk.TOP, fill=tk.X, pady=10)
 
-        self.return_buttons()
-        self.name_entry()
-        self.sec_name_entry()
-        self.lastname_entry()
-        self.ssn_entry()
-        self.email_entry()
-        self.place_entry()
-        self.field_listbox()
-        self.submit()
 
-    def return_buttons(self):
-        btn_home = tk.Button(
-            self,
-            text="Return to Home Page",
-            command=lambda: self.controller.show_frame("StartPage")
-        )
+    def return_button(self):
         btn_return = tk.Button(
             self,
             text="return",
-            command=lambda: self.controller.show_frame("StudentPage")
+            command=lambda: self.return_refresh()
         )
-        btn_home.pack()
         btn_return.pack()
 
-    def name_entry(self):
-        self.f_name = tk.Frame(master=self)
-        self.l_name = tk.Label(master=self.f_name, text="name")
-        self.l_name.pack()
-        self.e_name = tk.Entry(master=self.f_name)
-        self.e_name.pack()
-        self.f_name.pack()
 
-    def sec_name_entry(self):
-        self.f_sec_name = tk.Frame(master=self)
-        self.l_sec_name = tk.Label(master=self.f_sec_name, text="second name")
-        self.l_sec_name.pack()
-        self.e_sec_name = tk.Entry(master=self.f_sec_name)
-        self.e_sec_name.pack()
-        self.f_sec_name.pack()
+    def home_button(self):
+        btn_home = tk.Button(
+            self,
+            text="Return to Home Page",
+            command=lambda: self.home_refresh()
+        )
+        btn_home.pack()
 
-    def lastname_entry(self):
-        self.f_lastname = tk.Frame(master=self)
-        self.l_lastname = tk.Label(master=self.f_lastname, text="lastname")
-        self.l_lastname.pack()
-        self.e_lastname = tk.Entry(master=self.f_lastname)
-        self.e_lastname.pack()
-        self.f_lastname.pack()
 
-    def ssn_entry(self):
-        self.f_ssn = tk.Frame(master=self)
-        self.l_ssn = tk.Label(master=self.f_ssn, text="ssn")
-        self.l_ssn.pack()
-        self.e_ssn = tk.Entry(master=self.f_ssn)
-        self.e_ssn.pack()
-        self.f_ssn.pack()
+    def return_refresh(self):
+        self.refresh()
+        self.controller.show_frame("StudentPage")
 
-    def email_entry(self):
-        self.f_email = tk.Frame(master=self)
-        self.l_email = tk.Label(master=self.f_email, text="email")
-        self.l_email.pack()
-        self.e_email = tk.Entry(master=self.f_email)
-        self.e_email.pack()
-        self.f_email.pack()
 
-    def place_entry(self):
-        self.f_place = tk.Frame(master=self)
-        self.l_place = tk.Label(master=self.f_place, text="place of residence")
-        self.l_place.pack()
-        self.e_place = tk.Entry(master=self.f_place)
-        self.e_place.pack()
-        self.f_place.pack()
+    def home_refresh(self):
+        self.refresh()
+        self.controller.show_frame("StartPage")
 
-    def field_listbox(self):
-        self.f_field = tk.Frame(master=self)
-        self.l_field = tk.Label(master=self.f_field, text="field of study")
-        self.l_field.pack()
-        self.list_field = tk.Listbox(master=self.f_field)
-        
-        for i, field in enumerate(self.controller.fields):
-            self.list_field.insert(i,field.get_name())
-        
-        self.list_field.pack()
-        self.f_field.pack()
 
     def submit(self):
-        self.f_submit = tk.Frame(master=self)
-        self.sub_btn = tk.Button(
-            master=self.f_submit,
+        f_submit = tk.Frame(master=self)
+        f_submit.pack()
+
+        sub_btn = tk.Button(
+            master=f_submit,
             text="submit",
             command=lambda:self.update_student()
             )
-        self.sub_btn.pack()
-        self.f_submit.pack()
+        sub_btn.pack()
+        
+
+    def fill_entry(self):
+        self.e_name.insert(tk.END, str(self.student.get_name()))
+        self.e_lastname.insert(tk.END, str(self.student.get_lastname()))
+        self.e_email.insert(tk.END, str(self.student.get_email()))
+        self.e_sec_name.insert(tk.END, str(self.student.get_sec_name()))
+        self.e_place.insert(tk.END, str(self.student.get_place_of_residence()))
+        self.e_ssn.insert(tk.END, str(self.student.get_ssn()))
+
+
+    def refresh(self):
+        self.e_name.delete(0, tk.END)
+        self.e_lastname.delete(0, tk.END)
+        self.e_email.delete(0, tk.END)
+        self.e_sec_name.delete(0, tk.END)
+        self.e_place.delete(0, tk.END)
+        self.e_ssn.delete(0, tk.END)
+
 
     def set_student(self, student):
         self.student = student
 
+
     def update_student(self):
+        self.set_attr_student()
+        self.student.update(self.controller.db)
+        self.controller.db.commit_conn()
+
+        #config after update
+        self.refresh()
+        self.controller.frames["StudentPage"].restart()
+
+    def set_attr_student(self):
         for field in self.controller.fields:
             if self.list_field.get(tk.ACTIVE) == field.get_name():
                 self.student.set_field_of_study(field)
@@ -352,92 +383,3 @@ class ChangeStudentPage(tk.Frame):
         self.student.set_ssn(int(self.e_ssn.get()))
         self.student.set_email(self.e_email.get())
         self.student.set_place_of_residence(self.e_place.get())
-
-        self.student.update(self.controller.db)
-        self.controller.db.commit_conn()
-        self.controller.show_frame("StudentPage")
-        self.controller.frames["StudentPage"].restart()
-
-class MultiListBox(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
-
-        self.data = (
-            ('id', 10),
-            ('name', 20),
-            ('second name', 20),
-            ('lastname', 20),
-            ('ssn', 20),
-            ('email', 20),
-            ('field of study', 30),
-            ('place of residence', 40)
-        )
-        self.lists = []
-        self.l = {}
-
-        for name, width in self.data:
-            temp_frame = tk.Frame(self)
-            temp_frame.pack(
-                side=tk.LEFT,
-                expand=tk.YES,
-                fill=tk.BOTH
-                )
-
-            tk.Label(
-                master=temp_frame,
-                text=name,
-                borderwidth=1,
-                relief=tk.RAISED
-            ).pack(fill=tk.X)
-
-            lb = tk.Listbox(
-                master=temp_frame,
-                width=width,
-                borderwidth=0,
-                selectborderwidth=0,
-                relief=tk.FLAT,
-                exportselection=tk.FALSE
-            )
-            lb.pack(expand=tk.YES, fill=tk.BOTH)
-
-            self.lists.append(lb)
-            self.l[name] = lb
-
-            lb.bind('<B1-Motion>',lambda e, s=self: s._select(e.y))
-            lb.bind('<Button-1>',lambda e, s=self: s._select(e.y))
-
-        frame = tk.Frame(self)
-        frame.pack(side=tk.LEFT, fill=tk.Y)
-        tk.Label(
-            master=frame,
-            borderwidth=1,
-            relief=tk.RAISED
-        ).pack(fill=tk.X)
-
-    def _select(self, y):
-        row = self.lists[0].nearest(y)
-        self.selection_clear(0, tk.END)
-        self.selection_set(row)
-        return 'break'
-
-    def selection_set(self, first, last=None):
-        for l in self.lists:
-            l.selection_set(first, last)
-
-    def selection_clear(self, first, last=None):
-        for l in self.lists:
-            l.selection_clear(first, last)
-
-    def insert(self, index, *elements):
-        for e in elements:
-            i = 0
-            for name in self.lists:
-                name.insert(index, e[i])
-                i += 1
-
-    def delete(self, first, last=None):
-        for name in self.lists:
-            name.delete(first, last)
-
-    def index(self, index):
-        return self.lists[0].index(index)
---
+++ Frames/extendTk.py
@@ -0,0 +1,132 @@
+import tkinter as tk
+from apply import apply
+
+class MultiListBox(tk.Frame):
+    def __init__(self, master, data):
+        tk.Frame.__init__(self, master)
+        self.lists = []
+
+        for name, width in data:
+            temp_frame = tk.Frame(self)
+            temp_frame.pack(
+                side=tk.LEFT,
+                expand=tk.YES,
+                fill=tk.BOTH
+                )
+
+            tk.Label(
+                master=temp_frame,
+                text=name,
+                borderwidth=1,
+                relief=tk.RAISED
+            ).pack(fill=tk.X)
+
+            lb = tk.Listbox(
+                master=temp_frame,
+                width=width,
+                borderwidth=0,
+                selectborderwidth=0,
+                relief=tk.FLAT,
+                exportselection=tk.FALSE,
+                selectmode=tk.SINGLE
+            )
+            lb.pack(expand=tk.YES, fill=tk.BOTH)
+
+            self.lists.append(lb)
+            
+            lb.bind('<B1-Motion>',lambda e, s=self: s._select(e.y))
+            lb.bind('<Button-1>',lambda e, s=self: s._select(e.y))
+            lb.bind('<Leave>', lambda e: 'break')
+            lb.bind('<B2-Motion>', lambda e, s=self: s._b2motion(e.x, e.y))
+            lb.bind('<Button-2>', lambda e, s=self: s._button(e.x, e.y))
+            lb.bind('<Button-4>', lambda e, s=self: s._scroll(tk.SCROLL, -1, tk.PAGES))
+            lb.bind('<Button-5>',lambda e, s=self: s._scroll(tk.SCROLL, -1, tk.PAGES))
+            
+        frame = tk.Frame(self)
+        frame.pack(side=tk.LEFT, fill=tk.Y)
+        tk.Label(
+            master=frame,
+            borderwidth=1,
+            relief=tk.RAISED
+        ).pack(fill=tk.X)
+        sb = tk.Scrollbar(frame, orient=tk.VERTICAL, command=self._scroll)
+        sb.pack(expand=tk.YES, fill=tk.Y)
+        self.lists[0]['yscrollcommand']=sb.set
+
+    def _select(self, y):
+        row = self.lists[0].nearest(y)
+        self.selection_clear(0, tk.END)
+        self.selection_set(row)
+        return 'break'
+
+    def _button2(self, x, y):
+        for l in self.lists:
+            l.scan_mark(x, y)
+        return 'break'
+
+    def _b2motion(self, x, y):
+        for l in self.lists:
+            l.scan_dragto(x, y)
+        return 'break'
+
+    def _scroll(self, *args):
+        for l in self.lists:
+            apply(l.yview, args)
+        return 'break'
+
+
+    def curselection(self):
+        return self.lists[0].curselection()
+
+
+    def get(self, first, last=None):
+        result=[]
+        for l in self.lists:
+            result.append(l.get(first, last))
+        if last:
+            return apply(map, [None] + result)
+        return result
+
+
+    def selection_set(self, first, last=None):
+        for l in self.lists:
+            l.selection_set(first, last)
+
+
+    def selection_clear(self, first, last=None):
+        for l in self.lists:
+            l.selection_clear(first, last)
+
+
+    def insert(self, index, *elements):
+        for e in elements:
+            i = 0
+            for l in self.lists:
+                l.insert(index, e[i])
+                i += 1
+
+
+    def delete(self, first, last=None):
+        for l in self.lists:
+            l.delete(first, last)
+
+
+    def index(self, index):
+        return self.lists[0].index(index)
+
+
+    def size(self):
+        return self.lists[0].size()
+
+
+    def see(self, index):
+        for l in self.lists:
+            l.see(index)
+
+    def selection_anchor(self, index):
+        for l in self.lists:
+            l.selection_anchor(index)
+
+
+    def selection_includes(self, index):
+        return self.lists[0].selection_includes(index)
